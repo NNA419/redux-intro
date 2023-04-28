ebcc6abbbb0b99cee6ce6c675c2aa21d4f69ae98
NNA419 <aby.ngoc.anh@gamil.com>
2023-04-28 12:19:25 +0700

    upload redux-intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PropDrillingExercise from "./pages/PropDrillingExercise";
+import NotFoundPage from "./pages/NotFoundPage";
+import ReduxExercise from "./pages/ReduxExercise";
+import PropDrillingFinal from "./pages/PropDrillingFinal";
+import ReduxFinal from "./pages/ReduxFinal";
+import PublicLayout from "./layout/PublicLayout";
+import MThemeProvider from "./theme/MThemProvider";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MThemeProvider>
+        <Routes>
+          <Route path="/" element={<PublicLayout />}>
+            <Route index element={<PropDrillingExercise />} />
+            <Route path="prop-final" element={<PropDrillingFinal />} />
+            <Route path="redux-exercise" element={<ReduxExercise />} />
+            <Route path="redux-final" element={<ReduxFinal />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </MThemeProvider>
+    </BrowserRouter>
   );
 }
 
